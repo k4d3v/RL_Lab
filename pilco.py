3675790d99b0ae1c8894c4e6ee903dace8329520
kolev <mr_kolev95@gmx.de>
2018-11-15 12:48:59 +0100

    Adjusted algo structure so that it fits the structure of the algo in the doc

--- pilco.py
+++ pilco.py
@@ -4,36 +4,38 @@ import numpy as np
 
 
 class PILCO:
-    def __init__(self, N=1):
+    def __init__(self, J=1, N=1):
         """
-
-        :param N: Number of rollouts
+        :param J: Number of rollouts
+        :param N: Number of iterations
         """
+        self.J = J
         self.N = N
 
     def train(self):
         # Init.
 
-        # Sample controller params
-        Theta = np.random.normal(size=(self.N, self.N))
+        for j in range(self.J):
+            # Sample controller params
+            Theta = np.random.normal(size=(self.N, self.N))
 
-        # Apply random control signals and record data
+            # Apply random control signals and record data
 
-        while True:
+        for n in range(self.N):
             # Learn GP dynamics model using all data (Sec. 3.1)
 
-            while True:
-                # Approx. inference for policy evaluation (Sec. 3.2)
 
-                # Get J^pi(Theta) (9), (10), (11)
+            # Approx. inference for policy evaluation (Sec. 3.2)
 
-                # Policy improvement based on the gradient (Sec. 3.3)
-                # Get the gradient of J (12)-(16)
+            # Get J^pi(Theta) (9), (10), (11)
 
-                # Update Theta (CG or L-BFGS)
+            # Policy improvement based on the gradient (Sec. 3.3)
+            # Get the gradient of J (12)-(16)
 
-                # Convergence check
-                break
+            # Update Theta (CG or L-BFGS)
+
+            # Convergence check
+
 
             # Set new optimal policy
 
