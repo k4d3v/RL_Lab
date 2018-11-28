8a4121e9c6e344d0f1e9e4b4845f388dc5a9b02c
kolev <mr_kolev95@gmx.de>
2018-11-28 14:19:54 +0100

    Added plotting of cumulative rewards TODO: Compute cumulative reward in Value and Policy Iteration

---
+++ cumul_rew_measure.py
@@ -0,0 +1,48 @@
+""" Trains with different state and action space discretizations and compares the results"""
+
+import pickle
+import gym
+import quanser_robots
+import matplotlib.pyplot as plt
+
+from dyn_prog import DynProg
+from policy import RandomExplorationPolicy
+
+
+def compare_rewards(env_name, max_samples):
+    print(env_name)
+    # Open dynamics and reward NNs
+    reward = pickle.load(open("nets/rew_" + env_name + ".fitnn", 'rb'))
+    dynamics = pickle.load(open("nets/dyn_" + env_name + ".fitnn", "rb"))
+
+    env = gym.make(env_name)
+    policy = RandomExplorationPolicy()
+
+    discrets = range(1000, max_samples, 1000)
+    for discret in discrets:
+        print("Number of discrete entries: ", discret)
+        agent = DynProg(policy, env, reward, dynamics)
+        Vk1, pol1, cumul_rew1 = agent.train_val_iter(discret)
+        Vk2, pol2, cumul_rew2 = agent.train_pol_iter(discret)
+
+        # Make it pretty!
+        plt.plot(discrets, cumul_rew1, label="Value iteration")
+        plt.plot(discrets, cumul_rew2, label="Policy iteration")
+        plt.title("Comparison of learning for different dicretizations")
+        plt.xlabel("Number of discrete entries in the state and action space")
+        plt.ylabel("Cumulative reward after training")
+        plt.legend()
+
+        # Save plot
+        fig = plt.gcf()
+        fig.savefig('figures/cumul_rew_comparison_' + env_name + '.png')
+
+        # Show plot
+        plt.show()
+
+
+# Pendulum
+compare_rewards("Pendulum-v2", 10000)
+
+# Qube
+# compare_rewards("Qube-v0", 25000)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import gym
 import quanser_robots
+import pickle
 
 from dyn_prog import DynProg
 from policy import RandomExplorationPolicy
@@ -17,12 +18,17 @@ s_dim = env.reset().shape[0]
 reward = FitNN(s_dim+1, 1)
 dynamics = FitNN(s_dim+1, s_dim)
 
+# Sample training data
 print("Rollout policy...")
-points = reward.rollout(100, env)
+points = reward.rollout(2000, env) # See plots in figures dir for optimal number of samples
 
+# Learn dynamics and rewards
 reward.learn(False, points)
 dynamics.learn(True, points)
-# TODO: Plot accuracy of the model for different numbers of samples
+
+# Save for later use
+pickle.dump(reward, open("nets/rew_Pendulum-v2.fitnn", 'wb'))
+pickle.dump(dynamics, open("nets_Pendulum-v2/dyn.fitnn", 'wb'))
 
 agent = DynProg(policy, env, reward, dynamics)
 Vk, pol = agent.train_val_iter()
