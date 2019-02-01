Added latent function variance and noise; Fixed some bugs; EWs of Sigma_delta still negative, though

dyn_model.py
@@ -4,7 +4,7 @@ from scipy.optimize import minimize
 from torch.autograd import Variable, grad
 from torch.distributions import Normal
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 from matplotlib import pyplot as plt
 
 
@@ -12,6 +12,7 @@ class DynModel:
     """
     Gaussian Process with a squared exponential kernel for learning the system dynamics
     """
+
     def __init__(self, s_dim, data, lambs=None, beta=0.0001):
         """
         :param s_dim: Dimension of states
@@ -28,7 +29,7 @@ class DynModel:
         self.alpha = 1
         self.beta = beta
 
-        self.mean = [] # TODO: Compute (?)
+        self.mean = []  # TODO: Compute (?)
         self.cov_f = self.squared_expo_kernel
         self.setup_sigma()
         self.fit_gp()
@@ -42,8 +43,8 @@ class DynModel:
         :param lambs: Custom length-scales (l^2)
         """
         if lambs is None:
-            lambs = np.array([self.s_dim+1]*(self.s_dim+1))
-        return (self.alpha**2)*np.exp(-1 / 2.0 * np.sum(np.power((x - y), 2)/lambs))
+            lambs = np.array([self.s_dim + 1] * (self.s_dim + 1))
+        return (self.alpha ** 2) * np.exp(-1 / 2.0 * np.sum(np.power((x - y), 2) / lambs))
 
     def calculate_sigma(self, x, cov_f, lambs=None):
         """
@@ -90,7 +91,7 @@ class DynModel:
         m_expt = (k_xX.T * sigmaI) * np.mat(self.y)
         # sigma_expt = k_xx - (k_xX.T * np.mat(self.sigma).I) * k_xX
         sigma_expt = k_xx + self.beta - (k_xX.T * sigmaI) * k_xX
-        return np.array(m_expt).reshape(-1,), np.array(sigma_expt).reshape(-1,)
+        return np.array(m_expt).reshape(-1, ), np.array(sigma_expt).reshape(-1, )
 
     def prepare_data(self, data):
         """
@@ -100,10 +101,10 @@ class DynModel:
         """
         x, y = [], []
         for traj in data:
-            for p in range(len(traj)-1):
+            for p in range(len(traj) - 1):
                 # Join state and action
                 x.append(np.concatenate((traj[p][0], traj[p][1])))
-                y.append(traj[p+1][0]-traj[p][0])
+                y.append(traj[p + 1][0] - traj[p][0])
         return x, y
 
     def estimate_hyperparams(self):
@@ -111,7 +112,7 @@ class DynModel:
         Estimates the optimal length-scales for the kernel by maximizing the marginal log-likelihood
         :return: optimal length-scales
         """
-        sig_n = 0.1**2 # TODO: Is it really needed?
+        sig_n = 0.1 ** 2  # TODO: Is it really needed?
         y = np.mat(self.y)
         n = self.sigma.shape[0]
 
@@ -125,10 +126,10 @@ class DynModel:
             log_prob = []
             # Compute mll for each dimension
             for d in range(self.s_dim):
-                data_fit = (-1/2)*y[:,d].T*KI*y[:,d]
-                det = (1/2)*np.log(np.linalg.det(K))
-                norm = -(n/2)*np.log(2*np.pi)
-                log_prob.append(data_fit-det-norm)
+                data_fit = (-1 / 2) * y[:, d].T * KI * y[:, d]
+                det = (1 / 2) * np.log(np.linalg.det(K))
+                norm = -(n / 2) * np.log(2 * np.pi)
+                log_prob.append(data_fit - det - norm)
             return np.mean(log_prob)
 
         """
@@ -158,10 +159,10 @@ class DynModel:
             """
 
         # Optimize marginal ll
-        init = [1]*(self.s_dim+1)
-        bounds = [(1e-9, None)]*(self.s_dim+1)
+        init = [1] * (self.s_dim + 1)
+        bounds = [(1e-9, None)] * (self.s_dim + 1)
         # TODO: Does minimization work right though?
-        optimal_lambs = minimize(mll, init, method='L-BFGS-B', bounds=bounds, options = {'disp': True}).x
+        optimal_lambs = minimize(mll, init, method='L-BFGS-B', bounds=bounds, options={'disp': True}).x
         return optimal_lambs
 
     def training_error(self):
@@ -173,7 +174,7 @@ class DynModel:
         for din, dout in zip(self.x, self.y):
             m, sig = self.predict(din)
             err.append(np.abs(dout - m))
-        return np.sum(err)/len(self.x)
+        return np.sum(err) / len(self.x)
 
     def training_error_gp(self):
         """
@@ -184,17 +185,21 @@ class DynModel:
         m = self.gp.predict(self.x)
         for i, dout in zip(range(len(self.x)), self.y):
             err.append(np.abs(dout - m[i]))
-        return np.sum(err)/len(self.x)
+        return np.sum(err) / len(self.x)
 
     def fit_gp(self):
         """
         Fits a sklearn GP based on the training data
         """
-        kern = RBF(length_scale=[1]*(self.s_dim+1))
+        kern = self.alpha * RBF(length_scale=[1] * (self.s_dim + 1), length_scale_bounds=np.array([1e-10, 1])) \
+               + WhiteKernel()
         gp = GaussianProcessRegressor(kern)
         gp.fit(self.x, self.y)
-        self.lambs = gp.kernel_.get_params()["length_scale"]
-        self.alpha = gp.alpha
+        # self.lambs = gp.kernel_.get_params()["length_scale"]
+        opti_params = gp.kernel_.get_params()
+        self.alpha = np.sqrt(opti_params["k1"].k1.constant_value)
+        self.lambs = opti_params["k1"].k2.length_scale
+        self.noise = opti_params["k2"].noise_level
         self.gp = gp
         print("GPML kernel: %s" % gp.kernel_)
         print(gp.kernel_.get_params())
@@ -211,7 +216,7 @@ class DynModel:
         # Plot the function, the prediction and the 95% confidence interval based on
         # the MSE
         for d in range(self.s_dim):
-            plt.subplot(self.s_dim+1, 1, d+1)
+            plt.subplot(self.s_dim + 1, 1, d + 1)
 
             x_d = np.array([ax[d] for ax in self.x])
             y_d = np.array([ay[d] for ay in self.y])
@@ -220,7 +225,7 @@ class DynModel:
                 x_test_d = np.array([x_test[d]])
                 y_pred_d = np.array([y_pred[0][d]])
                 plt.plot(x_d, y_d, 'b-', label=u'Training Data')
-                plt.errorbar(x_test_d.ravel(), y_pred_d, sigma, fmt='r.', markersize=10, label=u'Prediction')
+                plt.errorbar(x_test_d.ravel(), y_pred_d, sigma, fmt='g.', markersize=10, label=u'Prediction')
             else:
                 mu_d = np.array([ay[d] for ay in mu])
                 plt.plot(x_d, mu_d, 'b-', label=u'Prediction')
@@ -228,8 +233,7 @@ class DynModel:
                          np.concatenate([mu_d - 1.9600 * sig, (mu_d + 1.9600 * sig)[::-1]]),
                          alpha=.5, fc='b', ec='None', label='95% confidence interval')
 
-            plt.xlabel("In " +str(d))
-            plt.ylabel("Out "+str(d))
+            plt.xlabel("In " + str(d))
+            plt.ylabel("Out " + str(d))
             plt.legend(loc='lower left')
         plt.show()
-

pilco.py
@@ -160,10 +160,14 @@ class PILCO:
             apolicy.assign_Theta(param_array)
 
             # Generate initial test input
-            # TODO: Maybe generate inputs closer to prior distribution (How to get params?)
-            x0 = np.random.normal(size=dyn_model.s_dim + 1)
+            # Generate input close to prior distribution
+            mean_samp = np.mean(self.x_s, axis=0)
+            std_samp = np.std(self.x_s, axis=0)
+            x0 = np.random.multivariate_normal(mean_samp, np.diag(std_samp))
             # Get action from policy based on the state
-            x0[-1] = apolicy.get_action(x0[:-1])
+            lx0 = list(x0)
+            lx0.append(apolicy.get_action(x0))
+            x0 = np.array(lx0)
 
             pred_mu, pred_Sigma = dyn_model.gp.predict([x0], return_std=True)
             # Plot prediction
@@ -273,19 +277,19 @@ class PILCO:
         # self.Lambda = np.diag(length_scale)
         K, K_inv = [], []  # K for all input und dimension, each term is also a matrix for all input and output
         for a in range(D):
-            K_dim = np.ones((n, n))  # K_dim tmp to save the result of every dimension
+            K_dim = np.full((n, n), self.alpha)  # K_dim tmp to save the result of every dimension
             for i in range(n):
                 for j in range(i+1, n):
                     # (6)
                     curr_x = (self.x_s[i] - self.x_s[j]).reshape(-1, 1)
                     # self.x_s is x_schlange in paper,training input
                     #kern = (self.alpha[a] ** 2) * np.exp(-0.5 * ((self.x_s[i][a] - self.x_s[j][a]) ** 2) / length_scale[a])
-                    #kern = dyn_model.alpha * np.exp(-0.5 * (np.dot(np.dot(curr_x.T, self.Lambda_inv[a]), curr_x)))
+                    #kern = self.alpha * np.exp(-0.5 * (np.dot(np.dot(curr_x.T, self.Lambda_inv[a]), curr_x)))
                     kern = (self.alpha**2) * np.exp(-0.5 * (np.dot(np.dot(curr_x.T, self.Lambda_inv[a]), curr_x)))
                     K_dim[i][j] = kern
                     K_dim[j][i] = kern
             K.append(K_dim)
-            K_inv.append(np.linalg.inv(K_dim+dyn_model.alpha*np.eye(n)))
+            K_inv.append(np.linalg.inv(K_dim+dyn_model.noise*np.eye(n)))
 
         # calculate beta, under (14)
         # calculate mu_delta (14)
@@ -312,7 +316,7 @@ class PILCO:
                     Sigma_delta[a][b] = E_delta - mu_delta[a] * mu_delta[b]
                 else:
                     # (23)
-                    E_var = self.alpha ** 2 - np.trace(K_inv[a] * Q) # TODO: Too small
+                    E_var = (self.alpha ** 2) - np.trace(K_inv[a] * Q) + dyn_model.noise # TODO: Too small
                     # (17)=(23)+(20)- mu_delta_a**2
                     Sigma_delta[a][a] = E_var + E_delta - mu_delta[a] ** 2
 
@@ -330,6 +334,8 @@ class PILCO:
             cov[:, a] = acol
 
         print("Done approximating mu and sigma delta, ", timer()-start)
+        # Compute eigenvals for debugging
+        ew, _ = np.linalg.eig(Sigma_delta)
         return mu_delta, Sigma_delta, cov
 
     def compute_Q(self, a, b, v, mu_t, Sigma_t):
@@ -358,7 +364,7 @@ class PILCO:
         #Q_old = np.zeros((n, n))
         for i in range(n):
             ksi_i = self.x_s[i] - mu_t
-            for j in range(n):
+            for j in range(i, n):
                 # Deisenroth implementation (eq. 2.53)
                 ksi_j = self.x_s[j] - mu_t
                 z_ij = (np.dot(self.Lambda_inv[a], ksi_i) + np.dot(self.Lambda_inv[b], ksi_j)).reshape(-1,1)
@@ -370,7 +376,9 @@ class PILCO:
                 snd_3 = np.dot(np.dot(np.dot(z_ij.T, R_inv), Sigma_t), z_ij)
 
                 snd = 0.5 * (snd_1 + snd_2 - snd_3)
-                Q[i][j] = np.exp(fst-snd)/detsqrt
+                entry = np.exp(fst-snd)/detsqrt
+                Q[i][j] = entry
+                Q[j][i] = entry
 
                 """
                 curr_xi = (self.x_s[i] - mu_t).reshape(-1, 1)
@@ -399,5 +407,5 @@ class PILCO:
         length_scale = dyn_model.lambs
         self.Lambda = [np.diag(np.array([length_scale[a]] * self.D)) for a in range(self.D)]
         self.Lambda_inv = [np.linalg.inv(Lamb) for Lamb in self.Lambda]
-        self.alpha = 1  # alpha is (1, D) matrix ?
+        self.alpha = dyn_model.alpha
         self.x_s = [ax[:-1] for ax in dyn_model.x]  # Training data