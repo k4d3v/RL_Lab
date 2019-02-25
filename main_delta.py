cd0d9726a18c18424e9b5102f42c15fe1b275c49
kolev <mr_kolev95@gmx.de>
2019-02-25 12:28:08 +0100

    New policies, new fig

--- main_delta.py
+++ main_delta.py
@@ -50,6 +50,7 @@ for env_name in env_names:
     all_rews = []
     # Loop over different numbers of trajectory samples
     for ad in delta:
+        ad = round(ad, 4)
         print("Delta: "+str(ad))
         avg_rewards = []
 
@@ -106,7 +107,7 @@ for env_name in env_names:
     plt.figure(figsize=(4.6, 3.6))
     # Plot average rewards for different numbers of trajectories
     for ar in range(len(all_rews)):
-        plt.plot(num_iters, all_rews[ar], label="Delta: "+str(delta[ar]))
+        plt.plot(num_iters, all_rews[ar], label="Delta: "+str(ad))
     plt.xlabel("Number of Iterations")
     plt.ylabel("Average Reward on 100 Episodes")
     plt.title("Average Rewards for " + env_name)
